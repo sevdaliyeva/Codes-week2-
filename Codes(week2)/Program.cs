1999757e1866ce24a8e790cd298b0d5a4cb799fe
Reboot <sevdaia@code.edu.az>
2022-08-11 22:37:10 +0300

    Add project files.

---
+++ Codes(week2)/Program.cs
@@ -0,0 +1,173 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+public class Task
+{
+    public static void Main()
+    {
+
+        //    //1.Write a C# that calculates the day of week (ex: Monday) of your birthday
+    //    int day = dayofweek(14,09,1998);
+    //    Console.WriteLine(day);
+    //}
+    //static int dayofweek(int d, int m, int y)
+    //{
+    //    int[] t = { 0,3,3,6,1,4,6,2,5,0,3,5};
+    //    y -= (m < 3) ? 1 :0;
+    //    return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
+
+        //2.Write a C# that calculates you current life days (how many days past since your birth)
+        //DateTime myBirthday = DateTime.Parse("28/11/1997");
+        //TimeSpan myAge=DateTime.Now - myBirthday;
+        //Console.WriteLine("You are " + myAge.TotalDays + "days old!");
+        //Console.ReadLine();
+
+
+
+        //3.find the largest of three numbers
+        //int a, b, c;
+        //Console.WriteLine("Find the largest of three numbers:");
+        //Console.WriteLine("Input the first number:");
+        // a = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Input the second number:");
+        //b = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Input the third number:");
+        //c = Convert.ToInt32(Console.ReadLine());
+        //if (a > b)
+        //{
+        //    if (a > c)
+        //    {
+        //        Console.Write("The 1st Number is the greatest among three");
+        //    }
+        //    else
+        //    {
+        //        Console.Write("The 3rd Number is the greatest among three");
+        //    }
+        //}
+        //else if (b > c)
+        //    Console.Write("The 2nd Number is the greatest among three");
+        //else
+        //    Console.Write("The 3rd Number is the greatest among three");
+
+        //4.Write a program in C# to check whether a number is a palindrome or not.
+        //int num;
+        //Console.WriteLine("Enter a number:");
+        //num = Convert.ToInt32(Console.ReadLine()); 
+        //int d = num;
+        //int tersi = 0;
+        //while (d != 0)
+        //{
+        //    tersi = tersi * 10;
+        //    tersi += d % 10;
+        //    d = d / 10;
+        //}
+        //if (num == tersi)
+        //    Console.WriteLine("{0} is palindrome:",num);
+        //else
+        //    Console.WriteLine("{0} is not palindrome:",num);
+        //5.Write a program in C# Sharp which is a Menu-Driven Program to perform a simple calculation.
+        // int a, b;
+        // Console.WriteLine("Enter the first integer:");
+        // a = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Enter the second integer:");
+        // b = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Input your choice: 1,2,3,4,5");
+        // int result = 0;
+        //int operation =Convert.ToInt32(Console.ReadLine());
+        // switch (operation)
+        // {
+        //     case 1:
+        //         result = a + b;
+        //         Console.WriteLine("The Addition of {0} and {1} is: {2}",a, b, result);
+        //         break;
+        //         case 2:
+        //         result = a - b;
+        //         Console.WriteLine("The Substraction of {0} and {1} is: {2}", a, b, result);
+        //         break;
+        //     case 3:
+        //         result = a * b;
+        //         Console.WriteLine("The Multiplication of {0} and {1} is: {2}", a, b, result);
+        //         break;
+        //     case 4:
+        //         result = a / b;
+        //         Console.WriteLine("The Division of {0} and {1} is : {2}",a,b, result);
+        //         break;
+        //     case 5:
+        //         Console.WriteLine("Exit:");
+        //         break;
+        //        }
+
+        //6. create a function to calculate the sum of the individual digits of a given number
+        //int number;
+        //Console.Write("Enter a number: ");
+        //number = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("The sum of the digits of the number {0} is : {1}", number, SumDig(number));
+        // static int SumDig(int n)
+        //{
+        //    string n1 = Convert.ToString(n);
+        //    int sum = 0;
+        //    for (int i = 0; i < n1.Length; i++)
+        //        sum += Convert.ToInt32(n1.Substring(i, 1));
+        //    return sum;
+        //}
+        //7.Read any day number in integer and display day name in the word.
+        //int dayN;
+        //Console.Write("Input Day Number : ");
+        //dayN = Convert.ToInt32(Console.ReadLine());
+
+        //switch (dayN)
+        //{
+        //    case 1:
+        //        Console.Write("Monday");
+        //        break;
+        //    case 2:
+        //        Console.Write("Tuesday");
+        //        break;
+        //    case 3:
+        //        Console.Write("Wednesday");
+        //        break;
+        //    case 4:
+        //        Console.Write("Thursday");
+        //        break;
+        //    case 5:
+        //        Console.Write("Friday");
+        //        break;
+        //    case 6:
+        //        Console.Write("Saturday");
+        //        break;
+        //    case 7:
+        //        Console.Write("Sunday");
+        //        break;
+        //    default:
+        //        Console.Write("Wrong day number.Please enter 1 to 7");
+        //        break;
+        //}
+        //6.
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
